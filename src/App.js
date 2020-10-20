f2f5b32a0aa2ee01a9863b5f2c0f8103bb31c4ea
Maxx730 <max.kinghorn@gmail.com>
2020-10-20 10:20:37 -0400

    update

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import ReactImageScaler from 'react-image-scaler';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return(
+      <div>
+        <ReactImageScaler src='https://turbologo.com/articles/wp-content/uploads/2018/03/prozrachniy-logo-1-678x381.png' width={1200} height={900} backgroundColor={'#000000'} maxScale={3} scaleStep={0.01} onScaleApply={1}/>
+      </div>
+    );
+  }
 }
 
-export default App;
