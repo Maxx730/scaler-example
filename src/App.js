e0f4635d110b9d61aa7e2f8f544d6faa4203f26b
Maxx730 <max.kinghorn@gmail.com>
2020-11-11 13:03:47 -0500

    update

--- src/App.js
+++ src/App.js
@@ -1,17 +1,70 @@
 import React from 'react';
 import ReactImageScaler from 'react-image-scaler';
+import './index.css';
 
 export default class App extends React.Component {
   constructor(props) {
     super(props);
+
+    this.getFile = this.getFile.bind(this);
+
+    this.state = {
+      src: null,
+      grid: true,
+      maxScale: 3
+    }
   }
 
   render() {
     return(
-      <div>
-        <ReactImageScaler src='https://turbologo.com/articles/wp-content/uploads/2018/03/prozrachniy-logo-1-678x381.png' width={1200} height={900} backgroundColor={'#000000'} maxScale={3} scaleStep={0.01} onScaleApply={1}/>
+      <div className='react-scale-example'>
+        {!this.state.src && 
+        <div className='react-scale-settings'>
+          <h1>
+            React Scale Example
+          </h1>
+          <p>
+            Please choose a file to load into the scaler, choose scaler settings before file.
+          </p>
+          <input type="file" id="avatar" name="avatar" accept="image/png, image/jpeg" onChange={this.getFile}></input>
+          <ul className='options'>
+          <li className='option'>
+              <input type='checkbox' checked={this.state.grid} onChange={() => {
+                this.setState({
+                  grid: !this.state.grid
+                });
+              }}/>
+              <label>Draw Grid</label>
+            </li>
+            <li className='option'>
+              <input type='checkbox' checked={this.state.grid} onChange={(event) => {
+                this.setState({
+                  maxScale: event.target.value
+                });
+              }}/>
+              <label>Draw Grid</label>
+            </li>
+          </ul>
+        </div>
+        }
+        {this.state.src && <ReactImageScaler drawGrid={this.state.grid} src={this.state.src} backgroundColor={'#000000'} maxScale={this.state.maxScale} scaleStep={0.01} onScaleApply={(data) => {
+          data.then((result) => {
+            console.log(result);
+          });
+        }}/>}
       </div>
     );
+  }
+
+  getFile(event) {
+    const context = this;
+    const reader = new FileReader();
+    reader.onload = function(fsEvent) {
+      context.setState({
+        src: fsEvent.target.result
+      });
+    }
+    reader.readAsDataURL(event.target.files[0])
   }
 }
 
