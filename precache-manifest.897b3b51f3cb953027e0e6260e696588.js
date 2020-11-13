24cd60b01c72457df17c29cb20a957921000aea4
Maxx730 <max.kinghorn@gmail.com>
2020-11-13 10:55:53 -0500

    Updates

---
+++ precache-manifest.897b3b51f3cb953027e0e6260e696588.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f17a9ce2d52877e68496fd40b4bc1990",
+    "url": "/scaler-example/index.html"
+  },
+  {
+    "revision": "ce776100b4ec712091f3",
+    "url": "/scaler-example/static/css/main.2735b186.chunk.css"
+  },
+  {
+    "revision": "0cf6f44f2fdba54b7909",
+    "url": "/scaler-example/static/js/2.6c1d80c2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/scaler-example/static/js/2.6c1d80c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce776100b4ec712091f3",
+    "url": "/scaler-example/static/js/main.f7f9473b.chunk.js"
+  },
+  {
+    "revision": "a72a6cbb6174668c1314",
+    "url": "/scaler-example/static/js/runtime-main.68a3357b.js"
+  }
+]);
